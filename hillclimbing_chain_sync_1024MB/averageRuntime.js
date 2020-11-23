05f5e910f0321fb429326b87a2a6de4c8af2efa7
jzlai <jun-zhe.lai@spring-media.de>
2020-11-23 23:26:50 +0100

    more results

---
+++ hillclimbing_chain_sync_1024MB/averageRuntime.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const config1_Raw = fs.readFileSync(
+  "./configurations_hillclimbing_chain_sync_1024MB_1.json"
+);
+const config2_Raw = fs.readFileSync(
+  "./configurations_hillclimbing_chain_sync_1024MB_2.json"
+);
+const config3_Raw = fs.readFileSync(
+  "./configurations_hillclimbing_chain_sync_1024MB_3.json"
+);
+
+const config1 = JSON.parse(config1_Raw);
+const config2 = JSON.parse(config2_Raw);
+const config3 = JSON.parse(config3_Raw);
+const max1 = Math.max(
+  ...config1
+    .filter((entry) => !!entry.averageDuration)
+    .map((entry) => entry.averageDuration)
+);
+const max2 = Math.max(
+  ...config2
+    .filter((entry) => !!entry.averageDuration)
+    .map((entry) => entry.averageDuration)
+);
+const max3 = Math.max(
+  ...config3
+    .filter((entry) => !!entry.averageDuration)
+    .map((entry) => entry.averageDuration)
+);
+
+console.log(max1, max2, max3);
+
+const last1 = config1[config1.length - 1];
+const last2 = config2[config2.length - 1];
+const last3 = config3[config3.length - 1];
+
+const averageRuntime = Math.round(
+  (last1.averageDuration + last2.averageDuration + last3.averageDuration) / 3
+);
+console.log("average runtime", averageRuntime);
+console.log("max runtime", (max1 + max2 + max3) / 3);
+console.log(
+  "average iterations",
+  (config1.length + config2.length + config3.length) / 3
+);
---
+++ visualization/plot.py
@@ -0,0 +1,50 @@
+# imports
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+import os
+
+# data
+d_chain = {'Memory size': ["1024MB", "320MB", "1024MB", "320MB", "1024MB", "320MB", "1024MB", "320MB"],
+     'Type': ["Best case", "Best case", "Hill climbing", "Hill climbing", "Heuristic", "Heuristic", "Worst case", "Worst case"],
+     'runtime': [100, 200, 2571, 7261, 450, 550, 2701, 8350]}
+
+d_parallel = {'Memory size': ["1024MB", "320MB", "1024MB", "320MB", "1024MB", "320MB", "1024MB", "320MB"],
+     'Type': ["Best case", "Best case", "Hill climbing", "Hill climbing", "Heuristic", "Heuristic", "Worst case", "Worst case"],
+     'runtime': [100, 200, 3382, 9825, 450, 550, 4758, 19936]}
+
+df_chain = pd.DataFrame(data=d_chain)
+
+
+# assign colors to type using a dictionary
+colors = {'Best case':'steelblue',
+          'Worst case':'firebrick',
+          'Hill climbing':'darkkhaki',
+          'Heuristic': 'darkolivegreen'}
+
+fig_chain = px.bar(df_chain, y='runtime',  x='Memory size',  color="Type", barmode='group', labels={"runtime": "average execution time (ms)", "Type": ""}, color_discrete_map=colors)
+fig_chain['layout'].update(title = "Chain",
+                xaxis = dict(showgrid=False))
+fig_chain.update(layout=dict(title=dict(x=0.5)))
+
+fig_parallel = px.bar(d_parallel, y='runtime',  x='Memory size',  color="Type", barmode='group', labels={"runtime": "average execution time (ms)", "Type": ""}, color_discrete_map=colors)
+fig_parallel['layout'].update(title = "Parallel",
+                xaxis = dict(showgrid=False))
+fig_parallel.update(layout=dict(title=dict(x=0.5)))
+
+# legend
+legend = dict(
+    orientation="h",
+    yanchor="bottom",
+    y=-0.08,
+    xanchor="center",
+    x=0.5
+)
+fig_chain.update_layout(legend=legend)
+fig_parallel.update_layout(legend=legend)
+
+if not os.path.exists("images"):
+    os.mkdir("images")
+    
+fig_chain.write_image("images/fig_chain.png")
+fig_parallel.write_image("images/fig_parallel.png")
